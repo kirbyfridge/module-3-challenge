2c7701a390872099a7a0e0eab7a2bc92ca4197ff
kirbyfridge <114848971+kirbyfridge@users.noreply.github.com>
2022-10-31 23:53:41 -0400

    added prompts that set length and characters

--- script.js
+++ script.js
@@ -2,24 +2,79 @@
 
 // generatePassword is the function that creates the password
 function generatePassword() {
-// Defining max & min so length does not exceed
-    var max = 128;
-    var min = 8;
 
 // Characters available to use within our generated passwords
-    var char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    var lower = "abcdefghijklmnopqrstuvwxyz";
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var number = "1234567890";
+    var symbol = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    var availableChar = "";
 
-// Creating random number using the max * min to be the length of the password
-    var randomLength = Math.floor(Math.random() * (max - min + 1)) + min;
+// Prompt will pop up & ask for a maximum password length desired
+    var inputWin = window.prompt("Input your desired max password password length (between 8 - 128)");
+    var max = parseInt(inputWin);
 
-// Makes an array using randomLength, 
-// Fills it with elements within the string char, 
+// If the length the user input is below 8 or above 128, alert user to put another number then returns
+    if (max < 8 || max > 128) {
+        window.alert("Password must be between 8-128 characters long");
+        return;
+    }
+
+// If it is not a number, it will alert user to input number then returns
+    if (isNaN(max)) {
+        window.alert("Please input a numberical value");
+        return;
+    }
+
+// Prompt will pop up & ask for a minimum password length desired
+    var inputWin = window.prompt("Input your desired min password length (between 8 - 128)");
+    var min = parseInt(inputWin);
+
+// If the length the user input is below 8 or above 128, alert user to put another number then returns
+    if (min < 8 || min > 128) {
+        window.alert("Password must be between 8-128 characters long");
+        return;
+    }
+
+// If it is not a number, it will alert user to input number then returns
+    if (isNaN(min)) {
+        window.alert("Please input a numberical value");
+        return;
+    }
+
+// Prompts that ask user if they would like to include lowercase, uppercase, numbers and/or symbols in their password 
+// then if true, concatenates all desired characters into one string
+    var confirmLower = window.confirm("Include lowercase letters?"); 
+        if (confirmLower === true) {
+            availableChar = availableChar.concat(lower);
+        };
+
+    var confirmUpper = window.confirm("Include uppercase letters?");
+        if (confirmUpper === true) {
+            availableChar = availableChar.concat(upper);
+        };
+
+    var confirmNumber = window.confirm("Include numbers?");
+        if (confirmNumber === true) {
+            availableChar = availableChar.concat(number);
+        };
+
+    var confirmSymbol = window.confirm("Include symbols?");
+        if (confirmSymbol === true) {
+            availableChar = availableChar.concat(symbol);
+        };
+
+// Creating random number using the max & min that the user has set within prompts
+    var passLength = Math.floor(Math.random() * (max - min + 1)) + min;
+
+// Makes an array using the randomized password length created using max & min, 
+// Fills it with elements within the string of available characters, 
 // Maps out function randomChar, which also creates a random number for each element in array
 // Return array as string that is readable as a password
-    var passCreate = Array(randomLength).fill(char).map(function(randomChar) 
-    { return randomChar[Math.floor(Math.random() * randomChar.length)] }).join('');
+    var passCreate = Array(passLength).fill(availableChar).map(function(randomChar) 
+    { return randomChar[Math.floor(Math.random() * randomChar.length)] }).join("");
     
-// returns newly created password
+// Returns newly created password
     return passCreate;
 }
 
